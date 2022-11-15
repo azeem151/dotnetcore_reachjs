0512e0aec261138d5ee1c0555ac9e33a19775f7e
Azeem Afzal <azeem@talentdesk.io>
2022-11-15 12:02:00 +0000

    removed white lines

--- ReachJs/Models/Pump.cs
+++ ReachJs/Models/Pump.cs
@@ -3,7 +3,6 @@ namespace ReachJs.Models
 {
     public class Pump
     {
-       
         public int Id { get; set; }
         public String Name { get; set; }
 
--- ReachJs/Models/Site.cs
+++ ReachJs/Models/Site.cs
@@ -6,9 +6,7 @@ namespace ReachJs.Models
     public class Site
     {
         public int Id { get; set; }
-
         public String Name { get; set; }
-
         public List<Pump> Pumps { get; set; }
    
     }
